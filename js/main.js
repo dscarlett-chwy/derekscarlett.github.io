c9764d60563599369ea8f159d17ce018faf476d9
DerekScarlett <36242561+DerekScarlett@users.noreply.github.com>
2018-03-09 12:36:28 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,70 +1,60 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Derek Scarlett</title>
-    <link rel="stylesheet" href= "css/style.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
-  </head>
-  <body>
-<section class="intro">
-  <img class="logo" src="Scarlett (1).png" alt="logo">
-</section>
+/**Personal
+Author: Derek SCarlett
+LIC: GPLv3
+*/
+$( ".logo" ).hover(function() {
+  $( this ).fadeOut( 500 );
+  $( this ).fadeIn( 500 );
+});
+$('.logo').click(() => {
 
-<section class="bio">
-  <section class="headShot">
-     <img src="headshot.jpg" alt="Head Shot">
-  </section>
-  <section class="aboutMe">
-    <h2 id="msg"></h2>
-    <p id="go"> Ever since I was young, I’ve played a variety of instruments ranging from trombone to percussion. In 2016, I completed my Bachelor’s degree in Music and Theater Arts. After graduation while looking for music jobs I took a full time gig at a telecommunications company to earn some money. While working here I felt like it stunted my growth--professionally and mentally. To continue developing myself professionally, I enrolled in a Master’s degree for Engineering Management, but that proved to be costly.  During my tenure there I went to a Blacks in computing conference in atlanta during the summer of 2017 and saw how there were endless paths you can take with tech. What sold me was hearing someone present about how they use sensors to track real time stats of basketball players that are logged straight into your ipad. This lead me to find out about how I could merge the newest technologies with my favorite subject-- music. It sparked a drive in me to go out and search for ways to get involved until I found Resilient coders. I’m grateful for Resilient Coders because they have taught me to harness my logical and creative sides and make it into a career. I am ready for the world to see who Derek Scarlett really is, and I hope you are too.
-    </p>
-    <section class="buttons">
-      <button id="shrink" type="button" name="button">Click!</button>
-      <button id="check" class="hidden" type="button" name="button"><a href="#">Check out my GitHub</a></button>
-      <button id="resume" class="hidden" oninvalid=""type="button" name="button">Check out my Resume</button>
-    </section>
-  </section>
-</section>
-<section class="new">
-  <img id="moneyMaker" class="hidden" src="Dereks Resume (6).jpg" alt="">
-  <section class="skills">
-    <h2>Skills</h2>
-    <ul>
-      <li>HTML</li>
-      <li>CSS</li>
-      <li>Javascript</li>
-      <li>JQuery</li>
-      <li>Node.js</li>
-      <li>Express</li>
-      <li>MongoDB</li>
-      <li>React</li>
-    </ul>
-  </section>
+    $('html, body').animate({
+        scrollTop: $('.bio').offset().top
+    }, 1000);
+    $(function () {
+     showText("#msg", "About Me!", 0, 275);
+});
+});
+$(window).scroll(function () {
+      if ($(this).scrollTop() > 300) {
+          $('.back-top').fadeIn();
+      } else {
+          $('.back-top').fadeOut();
+      }
+  });
 
-</section>
-<section class="back-top" title="Top of Page">
- <span id="back">Top</span>
-</section>
-<section class="email">
-  <h2>Need a Developer? Shoot me an email!</h2>
-  <p>Im always looking for work</p>
-  <form action="https://formspree.io/DerekScarlett009@gmail.com"
-       method="POST">
-     <input type="email" name="_replyto" placeholder="Your Email">
-     <input id="text" type="text" name="name" placeholder="Your Message">
-     <input id="send" type="submit" value="Send">
- </form>
-</section>
-<footer>
-  <section class="links">
-    <a href="https://github.com/DerekScarlett">GitHub!</a>
-    <a href="https://twitter.com/DerekScarlett3">Twitter!</a>
-    <a href="https://www.linkedin.com/in/derek-scarlett/">Linkedin!</a>
-    </section>
-  </footer>
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-      <script src="js/main.js"></script>
-    </body>
-  </html>
+  // scroll body to 0px on click
+  $('.back-top').click(function () {
+      $('body,html').animate({
+          scrollTop: 0
+      }, 1600);
+      return false;
+  });
+
+  var showText = function (target, message, index, interval) {
+
+    if (index < message.length) {
+      $(target).append(message[index++]);
+      setTimeout(function () { showText(target, message, index, interval); }, interval);
+    }
+
+  }
+$("#shrink").on("click", function(){
+  $("#go").toggle(1000);
+  $("#check").fadeIn(1000)
+  $("#resume").fadeIn(1000)
+  var jump = $("#check");
+  jump.animate({opacity:1});
+  jump.animate({height:'+=100px', width:'+=100px', top:'+=100px'},500);
+  jump.animate({height:'-=100px', width:'-=100px', left:'-=100px'},500);
+  jump.animate({height:'+=100px', width:'+=100px', top:'-=100px'},500);
+  jump.animate({height:'-=100px', width:'-=100px', left:'-=100px'},500);
+  jump.animate({opacity:0.5})
+})
+$("#resume").on("click", function(){
+  $('html, body').animate({
+      scrollTop: $('.new').offset().top
+  }, 1000);
+  $("#moneyMaker").slideToggle(500);
+})
+
